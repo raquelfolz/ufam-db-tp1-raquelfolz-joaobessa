fea16c0d525c2b0d0a628d4c79d27a41726d0528
Raquel Folz <raquel.folz@icomp.ufam.edu.br>
2023-04-16 17:30:09 -0400

    Add 2021/2 version files

---
+++ tp1_3.2.py
@@ -0,0 +1,230 @@
+# extração dos dados do arquivo de entrada,
+# criação do esquema do banco de dados
+# e povoamento das relações com estes dados
+
+import subprocess
+import psycopg2
+
+def fixquote(s):
+    l = []
+    for c in s:
+        if c != "'":
+            l.append(c)
+        else:
+            l.append("''")
+    return "".join(l)
+
+esquema = [
+"""CREATE TABLE PRODUTO(
+ASIN VARCHAR(10) PRIMARY KEY,
+ID_PRODUTO BIGINT,
+TITULO VARCHAR(500),
+GRUPO VARCHAR(20),
+avaliacao_media FLOAT,
+RANKING BIGINT
+); """,
+"""CREATE TABLE SIMILAR_PRODUTO(
+SIMILAR_ID SERIAL PRIMARY KEY,
+ASIN1_FK VARCHAR(10),
+ASIN2 VARCHAR(10),
+foreign key (ASIN1_FK) references PRODUTO (ASIN)
+); """,
+"""CREATE TABLE CATEGORIA(
+CATEGORIA_ID BIGINT PRIMARY KEY,
+CATEGORIA_ID_PAI BIGINT,
+NOME VARCHAR(300),
+foreign key (CATEGORIA_ID_PAI) references CATEGORIA (CATEGORIA_ID)
+); """,
+"""CREATE TABLE RELACAO_PRODUTO_CATEGORIA(
+RELACAO_ID SERIAL PRIMARY KEY,
+CATEGORIA_ID_FK BIGINT,
+ASIN_FK VARCHAR(10),
+foreign key (CATEGORIA_ID_FK) references CATEGORIA (CATEGORIA_ID),
+foreign key (ASIN_FK) references PRODUTO (ASIN)
+); """,
+"""CREATE TABLE REVIEW(
+data_review DATE,
+REVIEW_ID SERIAL PRIMARY KEY,
+ASIN_FK VARCHAR(10),
+AVALIACAO  FLOAT,
+VOTOS INT,
+UTILIDADE INT,
+id_cliente VARCHAR(30),
+foreign key (ASIN_FK) references PRODUTO (ASIN)
+ ); """
+]
+
+
+# deleta banco de dados se ja existir
+r = subprocess.run("psql -U postgres postgres -c \"DROP DATABASE tp1;\"",
+                   shell = True)
+# cria banco de dados
+r = subprocess.run("createdb tp1 -O postgres",
+                   shell = True)
+
+
+conn = psycopg2.connect("dbname=tp1 user=postgres host=postgres")
+
+# cria esquema do bd
+cur = conn.cursor()
+for table in esquema:
+    cur.execute(table)
+cur.close()
+conn.commit()
+
+# adiciona elementos do arquivo
+f = open('amazon-meta.txt', 'r', encoding="utf-8")
+fsimilar = open('.similar.txt', 'w', encoding="utf-8")
+fcategorias = open('.categorias.txt', 'w', encoding="utf-8")
+freview = open('.review.txt', 'w', encoding="utf-8")
+
+categorias = dict()
+
+cur = conn.cursor()
+
+line = f.readline()
+line = f.readline()
+itnum = int(line.split()[2])
+itcnt = 0
+
+while True:
+    try:
+        line = f.readline()
+    except EOFError:
+        break
+
+    if len(line) == 0:
+        continue
+
+    if line[:3] != "Id:":
+        continue
+
+    id = int(line[6:-1])
+    itcnt += 1
+
+    line = f.readline()
+    asin = line[6:-1]
+    if id % 1000 == 0:
+        print(id)
+
+    line = f.readline()
+    l = line[2:-1]
+    if l == "discontinued product":
+        sql = f"INSERT INTO produto(asin, id_produto) VALUES('{asin}', {id});"
+        cur.execute(sql)
+        continue
+    title = fixquote(l[7:])
+
+    line = f.readline()
+    group = fixquote(line[9:-1])
+
+    line = f.readline()
+    rank = int(line[12:-1])
+
+
+    #similar
+    line = f.readline().split()
+    l = line[2:]
+    for item in l:
+        print(asin, item, file=fsimilar)
+
+    #categories
+    line = f.readline().split()
+    catnum = int(line[1])
+    for i in range(catnum):
+        line = f.readline()[4:-1].split('|')
+        pai = "NULL"
+        for cat in line:
+            for i in range(len(cat)):
+                if cat[i] == '[':
+                    catn = i
+            cats = fixquote(cat[:catn])
+            catn = int(cat[catn+1:-1])
+            if not catn in categorias:
+                categorias[catn] = [cats, pai]
+            pai = catn
+
+        print(pai, asin, file=fcategorias)
+
+
+    #reviews
+    line = f.readline().split()
+    revnum = int(line[4])
+    avgrating = float(line[7])
+
+    for i in range(revnum):
+        line = f.readline().split()
+        date = line[0]
+        cliente = line[2]
+        rating = float(line[4])
+        votes = int(line[6])
+        helpful = int(line[8])
+
+        print(date, asin, rating, votes, helpful, cliente, file=freview)
+
+
+
+    sql = "INSERT INTO produto(asin, id_produto, titulo, grupo, ranking, avaliacao_media) " + \
+         f"VALUES('{asin}', {id}, '{title}', '{group}', {rank}, {avgrating});"
+    cur.execute(sql)
+
+    if itcnt >= itnum:
+        break
+
+cur.close()
+conn.commit()
+
+f.close()
+fsimilar.close()
+fcategorias.close()
+freview.close()
+
+print("Povoando categorias")
+cur = conn.cursor()
+for cat in categorias:
+    x = categorias[cat]
+    sql = "INSERT INTO categoria(categoria_id, categoria_id_pai, nome) " + \
+          f"VALUES({cat}, {x[1]}, '{x[0]}');"
+    cur.execute(sql)
+cur.close()
+conn.commit()
+
+print("Povoando similar")
+cur = conn.cursor()
+fsimilar = open('.similar.txt', 'r', encoding="utf-8")
+for line in fsimilar:
+    asin, item = line.split()
+    sql = "INSERT INTO similar_produto(asin1_fk, asin2) " + \
+         f"VALUES('{asin}', '{item}');"
+    cur.execute(sql)
+fsimilar.close()
+
+cur.close()
+conn.commit()
+
+print("Povoando relacao_produto_categoria")
+cur = conn.cursor()
+fcategorias = open('.categorias.txt', 'r', encoding="utf-8")
+for line in fcategorias:
+    cat, asin = line.split()
+    sql = "INSERT INTO relacao_produto_categoria(categoria_id_fk, asin_fk) " + \
+         f"VALUES({cat}, '{asin}');"
+    cur.execute(sql)
+fcategorias.close()
+cur.close()
+conn.commit()
+
+print("Povoando review")
+cur = conn.cursor()
+freview = open('.review.txt', 'r', encoding="utf-8")
+for line in freview:
+    date, asin, rating, votes, helpful, cliente = line.split()
+    sql = "INSERT INTO review(data_review, asin_fk, avaliacao, votos, utilidade, id_cliente) " + \
+         f"VALUES('{date}', '{asin}', {rating}, {votes}, {helpful}, '{cliente}');"
+    cur.execute(sql)
+freview.close()
+cur.close()
+conn.commit()
+
+
+conn.close()
---
+++ tp1_3.3.py
@@ -0,0 +1,127 @@
+# dashboard
+
+import subprocess
+
+
+helpstring = """
+-? a asin       | Exibe os 10 comentarios mais uteis no produto [asin].
+                    Os 5 primeiros têm a maior avaliação e os 5 últimos têm a menor.
+
+-? b asin       | Lista os produtos similares ao produto [asin] que são mais vendidos que ele.
+
+-? c asin       | Exibe a evolução por dia da média de avaliações do produto [asin].
+
+-? d              | Lista os 10 produtos mais vendidos de cada grupo.
+
+-? e              | Lista os 10 produtos com maior média de avaliações úteis positivas.
+
+-? f              | Lista as 5 categorias com maior média de avaliações úteis positivas por produto.
+
+-? g              | Lista 10 clientes que mais fizeram comentários por grupo de produto
+
+-? q              | Encerra programa
+"""
+
+print("\n\n\n")
+print(helpstring)
+
+while True:
+    try:
+        l = input('-? ')
+    except EOFError:
+        break
+
+    if l[0] == 'q':
+        break
+
+    if l[0] == 'a':
+        l = l.split()
+        try:
+            sql = """
+            SELECT * FROM
+            (SELECT data_review, avaliacao, votos,
+            utilidade, id_cliente FROM review
+            WHERE asin_fk = '"""  + l[1] + """'
+            ORDER BY utilidade DESC
+            FETCH FIRST 10 ROW ONLY) aux
+            ORDER BY avaliacao DESC, utilidade DESC;"""
+        except IndexError:
+            print("Comando precisa de ASIN do produto")
+            continue
+
+    if l[0] == 'b':
+        l = l.split()
+        try:
+            sql = """
+            SELECT b.asin, b.titulo, b.ranking
+            FROM produto a, produto b, similar_produto
+            WHERE
+            a.asin = '""" + l[1] + """' AND
+            a.asin = similar_produto.asin1_fk AND
+            b.asin = similar_produto.asin2 AND
+            b.ranking < a.ranking AND
+            b.ranking > 0;
+            """
+        except IndexError:
+            print("Comando precisa de ASIN do produto")
+            continue
+    if l[0] == 'c':
+        l = l.split()
+        try:
+            sql = """ SELECT data_review,
+            avg(avaliacao) AS avaliacao
+            FROM review WHERE
+            asin_fk = '""" + l[1] + """'
+            GROUP BY data_review
+            ORDER BY data_review;"""
+        except IndexError:
+            print("Comando precisa de ASIN do produto")
+            continue
+
+    if l[0] == 'd':
+        sql = """
+            SELECT * FROM
+            (((SELECT asin, titulo, ranking, grupo
+              FROM PRODUTO WHERE
+              grupo = 'Book' AND
+              ranking > -0
+              ORDER BY ranking
+              FETCH FIRST 10 ROW ONLY)
+            UNION
+            (SELECT asin, titulo, ranking, grupo
+              FROM PRODUTO WHERE
+              grupo = 'DVD' AND
+              ranking > 0
+              ORDER BY ranking
+              FETCH FIRST 10 ROW ONLY))
+            UNION
+            ((SELECT asin, titulo, ranking, grupo
+              FROM PRODUTO WHERE
+              grupo = 'Video' AND
+              ranking > 0
+              ORDER BY ranking
+              FETCH FIRST 10 ROW ONLY)
+            UNION
+            (SELECT asin, titulo, ranking, grupo
+              FROM PRODUTO WHERE
+              grupo = 'Music' AND
+              ranking > 0
+
+              ORDER BY ranking
+              FETCH FIRST 10 ROW ONLY))) aux
+
+            ORDER BY grupo, ranking;"""
+
+    if l[0] == 'e':
+        sql = """ SELECT asin, titulo,
+            avg(avaliacao) AS avaliacao FROM
+            (SELECT asin_fk, avaliacao FROM review WHERE
+            utilidade > 0 AND avaliacao > 0) aux,
+            produto
+            WHERE asin = asin_fk
+            GROUP BY asin
+            ORDER BY avaliacao DESC
+            FETCH FIRST 10 ROW ONLY;"""
+
+    r = subprocess.run("psql -U postgres tp1 -c \"" + sql + "\"",
+                       shell = True)
